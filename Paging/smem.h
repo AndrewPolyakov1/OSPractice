1270343219ef703f1fb38962b80f296b126cf7bd
AndrewPolyakov1 <mr.dijoy@gmail.com>
2022-02-01 18:15:36 +0300

    Moving from another rep

---
+++ Paging/smem.h
@@ -0,0 +1,17 @@
+#ifndef SMEM_H
+
+#define SMEM_H
+
+#include "common.h"
+
+u32int kmalloc_int(u32int sz, int align, u32int *phys);
+
+u32int kmalloc_a(u32int sz);
+
+u32int kmalloc_p(u32int sz, u32int *phys);
+
+u32int kmalloc_ap(u32int sz, u32int *phys);
+
+u32int kmalloc(u32int sz);
+
+#endif // SMEM_H
---
+++ lock.c
@@ -0,0 +1,36 @@
+//
+// lock.c -- Defines functions and structures for the locking mechanism.
+//           Written for JamesM's kernel development tutorials.
+//
+
+#include "lock.h"
+#include "common.h"
+#include "thread.h"
+#include "monitor.h"
+#include "scheduler.h"
+
+extern thread_t *current_thread;
+
+static uint32_t atomic_test_and_set (volatile spinlock_t *lock) {
+	register spinlock_t value = SPINLOCK_UNLOCKED;
+	
+	asm volatile("lock					\
+								xchgl	%0, %1"
+								: "=q" (value), "=m" (*lock)
+								: "0" (value));
+	
+  return value;
+}
+
+void spinlock_lock (spinlock_t *lock)
+{
+  while (atomic_test_and_set (lock) == SPINLOCK_LOCKED);
+  /*{
+    sleep();
+  }*/
+}
+
+void spinlock_unlock (spinlock_t *lock)
+{
+  *lock = 0;
+}
